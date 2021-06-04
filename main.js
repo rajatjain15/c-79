373008965b5557316cf636472faa6754c92d2374
rajatjain15 <79210039+rajatjain15@users.noreply.github.com>
2021-06-04 11:42:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+menu_list_array = ["Veg Margherita Pizza","Deluexe veggie pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+
+}
+
+function add_item(){
+var htmldata;
+
+
+}
+
+function add_top(){
+
+}
